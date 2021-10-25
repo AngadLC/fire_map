f8298c04533842308cf3b7227e82e3deda203635
Angad Lamichhane <71200732+AngadLC@users.noreply.github.com>
2021-10-25 16:22:48 +0545

    hazard index

--- Parbat_map/views.py
+++ Parbat_map/views.py
@@ -14,22 +14,16 @@ def project(request):
     # return HttpResponse("hi")
     return render(request,'project.html')
 def index(request):
-    print("hi")
-    geometry = ee.Geometry.Polygon([[[83.829803, 28.316455],[84.157677, 28.316455],[84.157677, 28.150463],
-        [83.829803, 28.150463]]])
-    print(geometry)
     context = {
-        "dem": dem(geometry),
+        "dem": dem(),
         "slope":slopefun(),
         "aspect":aspect(),
-        "lulc":lulc()
+        "lulc":lulc(),
+        'temp':temp(),
+        'firerisk':firerisk()
     }
     return render(request, 'index.html',context)
-def dem(geometry):
-    lulc1 = ee.ImageCollection("COPERNICUS/Landcover/100m/Proba-V-C3/Global").select('discrete_classification');
-    # print(lulc1)
-    properties = lulc1.propertyNames();
-    print(properties);
+def dem():
     image = ee.Image("USGS/SRTMGL1_003")
     viz_parameter = {'min':0,'max':3000,'palette': ['white','black','red']}
     map_id_dict = ee.Image(image).getMapId(viz_parameter)
@@ -45,11 +39,8 @@ def slopefun():
           .where(slopesmrt.gt(0).And(slopesmrt.lte(7)), 9) \
           .where(slopesmrt.gt(7).And(slopesmrt.lte(15)), 6) \
           .where(slopesmrt.gt(15).And(slopesmrt.lte(22)), 4)
-    viz_parameter = {'min':0,'max':20,'palette': ['white','black','red']}
-    map_id_dict = ee.Image(slopereclass).getMapId(viz_parameter)
-    tile = str(map_id_dict['tile_fetcher'].url_format)
-   
-    return tile
+    return slopereclass
+
 def aspect():
     image = ee.Image("USGS/SRTMGL1_003")
     aspect = ee.Terrain.aspect(image)
@@ -58,25 +49,76 @@ def aspect():
           .where(aspect.gt(7).And(aspect.lte(15)), 6) \
           .where(aspect.gt(15).And(aspect.lte(22)), 4)
     viz_parameter = {'min':0,'max':20,'palette': ['white','black','red']}
-    map_id_dict = ee.Image(aspect).getMapId(viz_parameter)
-    tile = str(map_id_dict['tile_fetcher'].url_format)
-    return tile
+    return aspectreclass
 def lulc():
-    lulc1 = ee.ImageCollection("COPERNICUS/Landcover/100m/Proba-V-C3/Global").select('discrete_classification')
-    # print(lulc1)
-    # lulcreclass = lulc1.map(func_cbg)
-    # viz_parameter = {'min':0,'max':200,'palette': ['white','black','red']}
-    # map_id_dict = ee.Image(lulcreclass).getMapId(viz_parameter)
-    # tile = str(map_id_dict['tile_fetcher'].url_format)
-    # return tile
+    reclass = ee.Image("ESA/GLOBCOVER_L4_200901_200912_V2_3").select('landcover')    
+    return reclass
+# temperature
+#cloud mask
+def temp():
+    def maskL8sr(col):
+        # Bits 3 and 5 are cloud shadow and cloud, respectively.
+        cloudShadowBitMask = (1 << 3)
+        cloudsBitMask = (1 << 5)
+        # Get the pixel QA band.
+        qa = col.select('pixel_qa')
+        # Both flags should be set to zero, indicating clear conditions.
+        mask = qa.bitwiseAnd(cloudShadowBitMask).eq(0) \
+                        .And(qa.bitwiseAnd(cloudsBitMask).eq(0))
+        return col.updateMask(mask)
+
     
+    col = ee.ImageCollection('LANDSAT/LC08/C01/T1_SR') \
+        .map(maskL8sr) \
+        .filterDate('2018-01-01','2018-12-31')
+    
+    #imagen reduction
+    image = col.median()
+    #median
+    ndvi = image.normalizedDifference(['B5',
+    'B4']).rename('NDVI')
+    #select thermal band 10(with brightness tempereature), no calculation
+    thermal= image.select('B10').multiply(0.1)
+    #fractional vegetation
+    fv =(ndvi.subtract(min).divide(max.subtract(min))).pow(ee.Number(2))
+    #Emissivity
+    a= ee.Number(0.004)
+    b= ee.Number(0.986)
+    EM=fv.multiply(a).add(b).rename('EMM')
+    #LST in Celsius Degree bring -273.15
+    LST = thermal.expression(
+        '(Tb/(1 + (0.00115* (Tb / 1.438))*log(Ep)))-273.15', {
+        'Tb': thermal.select('B10'),
+        'Ep': EM.select('EMM')
+        })
+    
+    # reclassify the land surface temperature
+    reclassifylst = ee.Image(1) \
+            .where(LST.gt(35),9) \
+            .where(LST.gt(30).And(LST.lte(35)), 8) \
+            .where(LST.gt(25).And(LST.lte(30)), 6) \
+            .where(LST.gt(20).And(LST.lte(25)), 5) \
+            .where(LST.gt(5).And(LST.lte(20)),3)
+    return reclassifylst
 
-def func_cbg (img):
+# fire risk module
 
-  return ee.Image(img) \
-    .where(img.eq(0).And(img.eq(50)), 9) \
-    .where(img.gt(50).And(img.lte(100)), 6) \
-    .where(img.gt(100).And(img.lte(150)), 4)
+def firerisk():
+    reclass,reclassifylst,slopereclass,reclassifyelevation,aspectreclass = aspect(),temp(),slopefun(), dem(), lulc()
+    fire_risk = ee.Image.expression(
+        '0.4*reclass +0.2*reclassifylst+0.15*slopereclass+0.15*reclassifyelevation+0.1*aspectreclass', {
+        'reclass': reclass,
+        'reclassifylst': reclassifylst,
+        'slopereclass': slopereclass,
+        'reclassifyelevation':reclassifyelevation,
+        'aspectreclass':aspectreclass
+    })
 
-
-
+    fire_risk_reclassify = ee.Image(1) \
+            .where( fire_risk.gt(7),4) \
+            .where(fire_risk.gt(5).And(fire_risk.lte(7)),3) \
+            .where( fire_risk.gt(3).And( fire_risk.lte(5)), 2) \
+            .where( fire_risk.gt(0).And( fire_risk.lte(3)), 1)
+    viz_parameter = {'min':0,'max':9,'palette': ['white','black','red']}
+    map_id_dict = ee.Image(fire_risk_reclassify).getMapId(viz_parameter)
+    tile = str(map_id_dict['tile_fetcher'].url_format)
